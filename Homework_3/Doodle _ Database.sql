e7e963ed899d42e90c2690b1b8db76a1a5e1d52f
Velislava02 <92212270+Velislava02@users.noreply.github.com>
2024-05-14 23:20:39 +0300

    Update Doodle _ Database.sql

--- Homework_3/Doodle _ Database.sql
+++ Homework_3/Doodle _ Database.sql
@@ -2,35 +2,28 @@ Use Users;
 
 create table Users
 ( id integer primary key,
-  username varchar(30),
-  password char(64)
+  username varchar(30) unique check(username like '%_%_%'),
+  password varchar(64) unique check(password like '%_._%_%')
 );
+
 
 create table Meeting
 ( id integer primary key,
   name varchar(50) unique,
-  start_date date,
-  end_date date,
-  start_time time,
-  end_time time
+  start_date date check(start_date >= '2024-01-01'),
+  end_date date check(end_date <= '2024-12-31'),
+  start_time time check(start_time >= '09:00:00'),
+  end_time time check (end_time <= '19:00:00')
 );
 
 create table Voting
 ( id integer primary key,
   user_id integer,
   meeting_id integer,
-  vote_date date,
-  vote_time time
+  vote_date date check(vote_date >= '2024-01-01' and vote_date <= '2024-12-31'),
+  vote_time time check(vote_time >= '09:00:00' and vote_time <= '19:00:00'),
+  foreign key (user_id) references Users(id),
+  foreign key (meeting_id) references Meeting(id)
 );
-
-alter table Voting add foreign key (user_id) references Users(id);
-alter table Voting add foreign key (meeting_id) references Meeting(id);
-
-alter table Users add constraint ck_password check(password <= 8 and password >= 16)
---alter table Users add constraint ck_username check(username >= 'a' and username <= 'z')
-alter table Meeting add constraint ck_mtg_data check(start_date >= '2024-01-01' and end_date <= '2024-12-31' and start_date <= end_date)
-alter table Meeting add constraint ck_mtg_time check(start_time >= '00:00:00' and end_time <= '23:59:59' and start_time <= end_time)
-alter table Voting add constraint ck_vt_date check(vote_date >= '2024-01-01' and vote_date <= '2024-12-31')
-alter table Voting add constraint ck_vt_time check(vote_time >= '00:00:00' and vote_time <= '23:59:59')
 
 
