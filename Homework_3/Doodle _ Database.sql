650e5657c0eef3c84c5082adae2b240a1ce2677f
Velislava02 <92212270+Velislava02@users.noreply.github.com>
2024-06-24 12:38:59 +0300

    Update Doodle _ Database.sql

--- Homework_3/Doodle _ Database.sql
+++ Homework_3/Doodle _ Database.sql
@@ -1,29 +1,34 @@
 Use Users;
 
 create table Users
-( id integer primary key,
-  username varchar(30) unique check(username like '%_%_%'),
-  password varchar(64) unique check(password like '%_._%_%')
+( user_id integer primary key,
+  username varchar(30) not null unique,
+  password varchar(64) not null,
+  email varchar(100) not null unique
 );
 
 
 create table Meeting
-( id integer primary key,
-  name varchar(50) unique,
+( meeting_id integer primary key,
+  title varchar(50) not null unique,
+  description text,
+  created_by integer,
   start_date date check(start_date >= '2024-01-01'),
   end_date date check(end_date <= '2024-12-31'),
   start_time time check(start_time >= '09:00:00'),
-  end_time time check (end_time <= '19:00:00')
+  end_time time check (end_time <= '19:00:00'),
+  foreign key (created_by) references Users(user_id)
 );
 
 create table Voting
-( id integer primary key,
+( vote_id integer primary key,
   user_id integer,
   meeting_id integer,
   vote_date date check(vote_date >= '2024-01-01' and vote_date <= '2024-12-31'),
   vote_time time check(vote_time >= '09:00:00' and vote_time <= '19:00:00'),
-  foreign key (user_id) references Users(id),
-  foreign key (meeting_id) references Meeting(id)
+  foreign key (user_id) references Users(user_id),
+  foreign key (meeting_id) references Meeting(meeting_id),
+  unique (user_id, meeting_id)
 );
 
 
